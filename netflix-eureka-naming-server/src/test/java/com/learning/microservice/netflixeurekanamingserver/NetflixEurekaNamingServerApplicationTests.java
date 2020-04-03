6feeca64f1509a02d963ddba2f87107ad8132f1e
FebinJose <febinjos007@gmail.com>
2020-04-04 09:24:01 +1300

    update package name

---
+++ netflix-eureka-naming-server/src/test/java/com/learning/microservice/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservice.netflixeurekanamingserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
